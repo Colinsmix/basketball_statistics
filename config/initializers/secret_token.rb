fd8bc8a33eeef6b7e67b846dcd86fbe41027477b
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-02-18 11:27:05 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasketballStatistics::Application.config.secret_key_base = '904749d597d56925deaea08cd8b145d1b9ac2a1df38ea4061a9235e5c874d122b6515fd19d5c4724625c7eb16f4d550c0997d8199dff4cea26a9794b83073b38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasketballStatistics::Application.config.session_store :cookie_store, key: '_basketball_statistics_session'
