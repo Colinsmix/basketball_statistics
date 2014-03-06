0b9598fe89c1eb0914051b6d9535035d8a9a5373
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-03-06 18:49:25 -0500

    D3 moved to Vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require vendor/d3
+//= require vendor/d3.min
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
